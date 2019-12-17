66f250bc200789b8fce068873cf1497e434c8549
nor0x <nor0x@users.noreply.github.com>
2019-12-17 17:43:34 +0100

    initial commit

---
+++ IndicatorViewRepro/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IndicatorViewRepro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Xamarin.Forms.Device.SetFlags(new[] {
+                "CarouselView_Experimental",
+                "IndicatorView_Experimental",
+                "SwipeView_Experimental"
+            });
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
